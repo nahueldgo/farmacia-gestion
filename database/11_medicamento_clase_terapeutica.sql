00f03295d71fc39c497a26d769f1fd26f75e6d77
Esper Amira Yasmin <yasesper85@gmail.com>
2026-09-13 12:55:04 -0300

    Agregamos tabla medicamento_clase_terapeutica al esquema

---
+++ database/11_medicamento_clase_terapeutica.sql
@@ -0,0 +1,6 @@
+CREATE TABLE medicamento_clase_terapeutica (
+    medicamento_id INT NOT NULL REFERENCES medicamento(producto_id) ON DELETE CASCADE,
+    clase_terapeutica_id INT NOT NULL REFERENCES clase_terapeutica(id_clase_terapeutica),
+    PRIMARY KEY (medicamento_id, clase_terapeutica_id)
+);
+
