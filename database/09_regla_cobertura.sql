b76f0537a0317e1c08754448b94549323efefd17
nahueldgo <nahueldgo@gmail.com>
2026-09-24 23:56:54 -0300

    Se agrego CHECK de rango a porcentaje_cobertura en ReglaCobertura

--- database/09_regla_cobertura.sql
+++ database/09_regla_cobertura.sql
@@ -2,7 +2,8 @@ CREATE TABLE regla_cobertura (
     id_regla_cobertura SERIAL PRIMARY KEY,
     obra_social_id INT NOT NULL REFERENCES obra_social(id_obra_social),
     categoria_cobertura_obra_social_id INT NOT NULL REFERENCES categoria_cobertura_obra_social(id_categoria_cobertura_obra_social),
-    porcentaje_cobertura NUMERIC(5,2) NOT NULL,
+    porcentaje_cobertura NUMERIC(5,2) NOT NULL
+        CHECK (porcentaje_cobertura BETWEEN 0 AND 100),
     activo BOOLEAN DEFAULT true,
     UNIQUE(obra_social_id, categoria_cobertura_obra_social_id)
 );
